a5dbe958ec63c054bebade8c7dae1bac37e96295
Vitalii Bielimov <vbit2000@gmail.com>
2024-03-04 20:45:41 +0200

    added functionality for note views

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('note/', views.note, name='note'),
     path('tag/', views.tag, name='tag'),
+    path('detail/<int:note_id>', views.detail, name='detail'),
 ]
