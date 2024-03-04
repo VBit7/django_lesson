e0996a52bf0d21ccf760289e08264e12be9741a4
Vitalii Bielimov <vbit2000@gmail.com>
2024-03-04 20:55:21 +0200

    added display of notes

--- notes/noteapp/urls.py
+++ notes/noteapp/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('note/', views.note, name='note'),
     path('tag/', views.tag, name='tag'),
     path('detail/<int:note_id>', views.detail, name='detail'),
+    path('done/<int:note_id>', views.set_done, name='set_done'),
+    path('delete/<int:note_id>', views.delete_note, name='delete'),
 ]
--- notes/noteapp/views.py
+++ notes/noteapp/views.py
@@ -6,7 +6,8 @@ from .models import Tag, Note
 
 # Create your views here.
 def main(request):
-    return render(request, 'noteapp/index.html')
+    notes = Note.objects.all()
+    return render(request, 'noteapp/index.html', {"notes": notes})
 
 
 def tag(request):
@@ -43,3 +44,13 @@ def note(request):
 def detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, 'noteapp/detail.html', {"note": note})
+
+
+def set_done(request, note_id):
+    Note.objects.filter(pk=note_id).update(done=True)
+    return redirect(to='noteapp:main')
+
+
+def delete_note(request, note_id):
+    Note.objects.get(pk=note_id).delete()
+    return redirect(to='noteapp:main')
